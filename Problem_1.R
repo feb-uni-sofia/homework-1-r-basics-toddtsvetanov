7b0288418aa31b883aab36bc8dfafa10ea17075d
toddtsvetanov <37773328+toddtsvetanov@users.noreply.github.com>
2018-04-02 17:20:52 +0300

    Add files via upload
    Тодор Цветанов, ф.н. 700870

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,26 @@
+# a)
+x <- c(4,1,1,4)
+
+# b)
+y <- c(1,4)
+
+# c) The result is like this because R Studio repeats "y" as many times as it takes
+# to fill "x" (it repeats it).
+x-y
+
+# d)
+s <- c(x,y)
+
+# e)
+rep(s,10)
+length(rep(s,10))
+
+# f)
+rep(s, each = 3)
+
+# g)
+seq(7,21) # i)  7:21 # ii)
+
+# h)
+length(seq(7,21))
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,36 @@
+# a)
+xmin <- c(23.0,20.5,28.2,20.3,22.4,17.2,18.2)
+xmax <- c(25.0,22.8,31.2,27.3,28.4,20.2,24.1)
+date <- c("03Mon18","04Tue18","05Wed18","04Thu18","05Fri18","06Sat18","07Sun18")
+
+# b)
+TempDiff <- abs(xmax - xmin)
+
+# c)
+mean(xmin)
+mean(xmax)
+
+# d)
+xmin[xmin < mean(xmin)]
+
+# e)
+xmin[xmin > mean(xmin)]
+
+# f)
+names(xmin) <- date
+names(xmax) <- date
+
+# g)
+temperatures <- data.frame(xmin,xmax)
+
+# h)
+temperatures <- within(temperatures, {xminFahrenheit <- (xmin*9/5+32) })
+
+# i)
+xminFahrenheit <- xmin*9/5+32
+xmaxFahrenheit <- xmax*9/5+32
+Fahrenheit <- data.frame(xminFahrenheit, xmaxFahrenheit)
+
+# j)
+j1Fahrenheit <- Fahrenheit[c(1:5),] # i)
 
